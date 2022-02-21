e573278d0c3346285072409a68f7799d5264f244
kasra-hosseini <khosseini@turing.ac.uk>
2022-02-21 15:57:33 +0000

    Add CI

---
+++ tests/test_import.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_workflow():
+    from scivision.io import load_pretrained_model, load_dataset
+
+    scivision_yml = 'https://raw.githubusercontent.com/alan-turing-institute/mapreader-plant-scivision/main/.scivision-config.yaml'
+    model = load_pretrained_model(scivision_yml, allow_install=True)
+
+    cat = load_dataset('https://github.com/alan-turing-institute/mapreader-plant-scivision')
+
+    ds = cat.plant_single().to_dask()
+
+    outputs = model.predict(ds[0])
+
+    print(outputs.head())
+
