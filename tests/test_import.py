c50849d008687fb97199916c4aa1b12e41d56485
Kasra Hosseini <khosseini@turing.ac.uk>
2022-03-22 11:24:13 +0000

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,18 +1,26 @@
 import pytest
 
+
 def test_workflow():
-    from scivision.io import load_pretrained_model, load_dataset
+    from scivision import default_catalog, load_pretrained_model, load_dataset
 
-    scivision_yml = 'https://raw.githubusercontent.com/alan-turing-institute/mapreader-plant-scivision/main/.scivision-config.yaml'
+    models_catalog = default_catalog.models.to_dataframe()
+    stp_repo = models_catalog[models_catalog.name == "mapreader-plant"].url.item()
     
-    model = load_pretrained_model(scivision_yml, allow_install=True)
-
-    cat = load_dataset('https://github.com/alan-turing-institute/mapreader-plant-scivision')
-
-    ds = cat.plant_single().to_dask()
-
-    outputs = model.predict(ds[0], plot_output=False)
-    assert len(outputs) == 30
+    model = load_pretrained_model(stp_repo, allow_install=True)
     
-    outputs = model.predict(ds[1], plot_output=False)
-    assert len(outputs) == 30
+    data_config = load_dataset('https://github.com/alan-turing-institute/mapreader-plant-scivision')
+    
+    # --- plant_single dataset 
+    plant_single = data_config.plant_single().to_dask()
+
+    model.predict(plant_single[0], slice_size=100)
+    
+    model.predict(plant_single[1], slice_size=100)
+
+    # --- plant_flower dataset 
+    plant_flower = data_config.plant_flower().to_dask()
+
+#     model.predict(plant_flower[0], slice_size=200)
+    
+#     model.predict(plant_flower[1], slice_size=200)
